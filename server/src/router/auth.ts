f1bc2f1d824423d2211193a049d9c6636b6be48d
Krysyx <anthololz31@yahoo.fr>
2021-03-14 22:11:06 +0100

    fixed ts-node transpilation errors

--- server/src/router/auth.ts
+++ server/src/router/auth.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 const router = Router();
 
-router.get("/", (req, res, next) => {});
+router.get("/", (req: any, res: any, next: any) => {
+  console.log("REQUEST HANDLED");
+});
 
 export default router;
