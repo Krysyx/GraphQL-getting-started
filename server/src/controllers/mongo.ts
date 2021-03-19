41924fdff9cce49ba596280dfdad723e857d9e9f
Krysyx <anthololz31@yahoo.fr>
2021-03-19 04:45:26 +0100

    created global author schema

--- server/src/controllers/mongo.ts
+++ server/src/controllers/mongo.ts
@@ -5,6 +5,9 @@ import {
   updateAuthorSchema,
 } from "../schema/graphQL";
 
+import { querySchema, mutationSchema } from "../schema/graphQL/AuthorSchema";
+
+//TODO: implement stacked queries and mutation instead of spreading them in multiple separated files
 const getAuthors = graphqlHTTP({ schema: getAuthorsSchema, graphiql: true });
 const create = graphqlHTTP({ schema: createAuthorSchema, graphiql: true });
 const update = graphqlHTTP({ schema: updateAuthorSchema, graphiql: true });
---
+++ server/src/schema/graphQL/AuthorSchema.ts
@@ -0,0 +1,61 @@
+import { Author } from "../mongoose";
+import {
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from "graphql";
+
+const type = new GraphQLObjectType({
+  name: "Author",
+  fields: {
+    _id: {
+      type: GraphQLString,
+    },
+    firstname: { type: GraphQLString },
+    lastname: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
+
+const query = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    getAuthors: {
+      type: new GraphQLList(type),
+      resolve: async () => await Author.find(),
+    },
+  },
+});
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createAuthor: {
+      type,
+      args: {
+        firstname: { type: GraphQLString },
+        lastname: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve: async (source, args) => await Author.create(args),
+    },
+    updateAuthor: {
+      type,
+      args: {
+        id: { type: GraphQLString },
+        firstname: { type: GraphQLString },
+        lastname: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve: async (source, { id, ...update }) =>
+        await Author.findByIdAndUpdate(id, update, { new: true }),
+    },
+  },
+});
+
+const querySchema = new GraphQLSchema({ query });
+const mutationSchema = new GraphQLSchema({ query: mutation });
+
+export { querySchema, mutationSchema };
