a992da3a6203617d79a10be9f88d0d8b2257343c
Krysyx <anthololz31@yahoo.fr>
2021-03-21 02:54:35 +0100

    fixed express static files

--- client/src/server.js
+++ client/src/server.js
@@ -1,5 +1,13 @@
-const express = require("express");
-const server = express();
+const dotenv = require("dotenv");
+dotenv.config({ path: `${process.env.NODE_ENV}.env` });
 
+const express = require("express");
+const path = require("path");
+const server = express();
+const PORT = process.env.PORT || 3000;
+
+server.use(express.static(path.resolve()));
 server.use(express.static(`${__dirname}/public`));
-server.listen(8080, () => console.log("Web server running on port 8080"));
+server.listen(PORT, () => console.log(`Web server running on port ${PORT}`));
+
+module.exports = process.env.PORT;
